e3c8094d891caad6f497050848e0c116c94d4921
Camila <camila.matosrocha1234@gmail.com>
2022-07-11 19:37:41 -0300

    Modo administrador configurado

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -26,6 +26,11 @@ export class TemaComponent implements OnInit {
       this.router.navigate(['/entrar'])
     }
 
+    if(environment.tipo != 'adm'){
+      this.alertas.showAlertWarning('Você precisa ser administrador para acessar essa rota')
+      this.router.navigate(['/inicio'])
+    }
+
     this.findAllTemas()
   }
 
