8af898bdf4e92214e53f94be0dadd4c3708e2c9b
Camila <camila.matosrocha1234@gmail.com>
2022-07-11 13:37:22 -0300

    PUT de usuário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UsuarioEditComponent } from './edit/usuario-edit/usuario-edit.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'entrar', pathMatch: 'full'},
@@ -22,6 +23,7 @@ const routes: Routes = [
   {path: 'tema-delete/:id', component: TemaDeleteComponent},
   {path: 'postagem-edit/:id', component: PostagemEditComponent},
   {path: 'postagem-delete/:id', component: PostagemDeleteComponent},
+  {path: 'usuario-edit/:id', component: UsuarioEditComponent},
 ];
 
 @NgModule({
